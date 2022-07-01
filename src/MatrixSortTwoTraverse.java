415d1e4d2c310b39168990e723b4ad5e4603a20b
Idan-Golan <idan.go@campus.technion.ac.il>
2022-07-01 17:26:14 +0300

    Implement MatrixSortTwoTraverse.

--- src/MatrixSortTwoTraverse.java
+++ src/MatrixSortTwoTraverse.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
 
+// MatrixSortTwoTraverse creates a list of the input matrix's elements making two traverses.
+// Traverse first even elements and then odd elements, starting count indices from 0.
 public class MatrixSortTwoTraverse<T> extends MatrixSorter<T> {
 
     /**
@@ -11,8 +14,23 @@ public class MatrixSortTwoTraverse<T> extends MatrixSorter<T> {
         super(width, height);
     }
 
+    /**
+     * @return A sorted form the elements in the given matrix.
+     *         First ascending order of even indices, then ascending order of odd indices.
+     */
     @Override
     public List<T> getSorted(T[][] matrix) {
-        return null;
+        List<T> sortedList = new ArrayList<>();
+        // traverse even indices (0,2,4,...)
+        for (int i = 0; i < _height; i++) {
+            for (int j = 0; j < _width; j += 2)
+                sortedList.add(matrix[i][j]);
+        }
+        // traverse odd indices (1,3,5,...)
+        for (int i = 0; i < _height; i++) {
+            for (int j = 1; j < _width; j += 2)
+                sortedList.add(matrix[i][j]);
+        }
+        return sortedList;
     }
 }
